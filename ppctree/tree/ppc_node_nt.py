1010356c49b39cc5f19dc4ee9cfa5a4b272e2f1b
Seth Kulick <skulick@ldc.upenn.edu>
2023-08-29 15:53:36 -0400

    ppc_node_nt fixup for  case

--- ppctree/tree/ppc_node_nt.py
+++ ppctree/tree/ppc_node_nt.py
@@ -4,10 +4,14 @@ from ppctree.tree.ppc_node import PPCNode
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
+# also allow for added _NT at end of bare label
 # bare label can have $ at end, like PRO$
-# also allow for _NT at end which gets added on
-RE_LABEL_TRACE = re.compile(r'^(?P<bare>[A-Z_]+\$?)(?P<ftags>(-[A-Z]+)+)*(-(?P<trace>[0-9]+))*$')
-RE_LABEL_GAP = re.compile(r'^(?P<bare>[A-Z_]+\$?)(?P<ftags>(-[A-Z]+)+)*(=(?P<gap>[0-9]+))*$')
+
+#RE_LABEL_TRACE = re.compile(r'^(?P<bare>[A-Z_]+\$?)(?P<ftags>(-[A-Z]+)+)*(-(?P<trace>[0-9]+))*$')
+#RE_LABEL_GAP = re.compile(r'^(?P<bare>[A-Z_]+\$?)(?P<ftags>(-[A-Z]+)+)*(=(?P<gap>[0-9]+))*$')
+
+RE_LABEL_TRACE = re.compile(r'^(?P<bare>[A-Z]+\$?(_NT)?)(?P<ftags>(-[A-Z]+)+)*(-(?P<trace>[0-9]+))*$')
+RE_LABEL_GAP = re.compile(r'^(?P<bare>[A-Z]+\$?(_NT)?)(?P<ftags>(-[A-Z]+)+)*(=(?P<gap>[0-9]+))*$')
 
 class PPCNodeNt(PPCNode):
     """Tree node that is a nonterminal.
